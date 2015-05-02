d4280706fbb32b3e8a4f40fe2d71c1cdada89626
ErikEJ <ejlskov@hotmail.com>
2015-05-02 14:25:54 +0200

    Initial checkin of solution structure

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("EntityFramework.SqlServerCompact")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EntityFramework.SqlServerCompact")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

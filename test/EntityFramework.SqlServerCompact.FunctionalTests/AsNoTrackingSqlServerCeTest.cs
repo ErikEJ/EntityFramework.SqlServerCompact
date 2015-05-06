0c7007eac35476e17ef860dd983e5facf04f1808
ErikEJ <ejlskov@hotmail.com>
2015-05-06 19:53:45 +0200

    Update to xunit 2.0.0 and added test fixture - Will let appveyor run the tests!

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/AsNoTrackingSqlServerCeTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.Entity.FunctionalTests;
+
+namespace ErikEJ.Data.Entity.SqlServerCe.FunctionalTests
+{
+    public class AsNoTrackingSqlServerCeTest : AsNoTrackingTestBase<NorthwindQuerySqlServerCeFixture>
+    {
+        public AsNoTrackingSqlServerCeTest(NorthwindQuerySqlServerCeFixture fixture)
+            : base(fixture)
+        {
+        }
+    }
+}

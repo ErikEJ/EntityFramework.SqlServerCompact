a45211279d905b18fd00332296c592079feaf05a
ErikEJ <ejlskov@hotmail.com>
2015-05-06 07:00:43 +0200

    Implement functional test fixtures fixes #9

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/NorthwindQuerySqlServerCeFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ErikEJ.Data.Entity.SqlServerCe.FunctionalTests.TestModels;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.FunctionalTests;
+using Microsoft.Data.Entity.FunctionalTests.TestModels.Northwind;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Relational.FunctionalTests;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+
+namespace ErikEJ.Data.Entity.SqlServerCe.FunctionalTests
+{
+    public class NorthwindQuerySqlServerCeFixture : NorthwindQueryRelationalFixture, IDisposable
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly DbContextOptions _options;
+        private readonly SqlServerCeTestStore _testStore;
+
+        public NorthwindQuerySqlServerCeFixture()
+        {
+            _testStore = SqlServerCeNorthwindContext.GetSharedStore();
+
+            _serviceProvider = new ServiceCollection()
+                .AddEntityFramework()
+                .AddSqlServerCe()
+                .ServiceCollection()
+                .AddSingleton(TestSqlServerCeModelSource.GetFactory(OnModelCreating))
+                .AddInstance<ILoggerFactory>(new TestSqlLoggerFactory())
+                .BuildServiceProvider();
+
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseSqlServerCe(_testStore.Connection.ConnectionString);
+            _options = optionsBuilder.Options;
+
+            _serviceProvider.GetRequiredService<ILoggerFactory>()
+                .MinimumLevel = LogLevel.Debug;
+        }
+
+        public override NorthwindContext CreateContext() => new SqlServerCeNorthwindContext(_serviceProvider, _options);
+
+        public void Dispose() => _testStore.Dispose();
+    }
+}
---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/TestSqlServerCeModelSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.FunctionalTests;
+using Microsoft.Data.Entity.Internal;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Metadata.Builders;
+using Microsoft.Framework.DependencyInjection;
+
+namespace ErikEJ.Data.Entity.SqlServerCe.FunctionalTests
+{
+    public class TestSqlServerCeModelSource : SqlServerCeModelSource
+    {
+        private readonly TestModelSource _testModelSource;
+
+        public TestSqlServerCeModelSource(Action<ModelBuilder> onModelCreating, IDbSetFinder setFinder, IModelValidator modelValidator)
+            : base(setFinder, modelValidator)
+        {
+            _testModelSource = new TestModelSource(onModelCreating, setFinder);
+        }
+
+        public static Func<IServiceProvider, ISqlServerCeModelSource> GetFactory(Action<ModelBuilder> onModelCreating) =>
+            p => new TestSqlServerCeModelSource(
+                onModelCreating,
+                p.GetRequiredService<IDbSetFinder>(),
+                p.GetRequiredService<IModelValidator>());
+
+        public override IModel GetModel(DbContext context, IModelBuilderFactory modelBuilderFactory) =>
+            _testModelSource.GetModel(context, modelBuilderFactory);
+    }
+}

4e943c9970f8258c277abab225e0e06519b14c68
ErikEJ <ejlskov@hotmail.com>
2015-05-10 07:45:35 +0200

    Adding another functional test

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/ChangeTrackingSqlServerCeTest.cs
@@ -0,0 +1,15 @@
+﻿using ErikEJ.Data.Entity.SqlServerCe.FunctionalTests;
+using Microsoft.Data.Entity.FunctionalTests;
+
+namespace ErikEJ.Data.Entity.SqlServerCompact.FunctionalTests
+{
+    public class ChangeTrackingSqlServerCeTest : ChangeTrackingTestBase<NorthwindQuerySqlServerCeFixture>
+    {
+        public ChangeTrackingSqlServerCeTest(NorthwindQuerySqlServerCeFixture fixture)
+            : base(fixture)
+        {
+        }
+    }
+}
+
+

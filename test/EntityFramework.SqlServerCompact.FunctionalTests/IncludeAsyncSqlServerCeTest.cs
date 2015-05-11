896d79a28f0bbb2f0d4e16f43f2b9e0be3c8e3c1
ErikEJ <ejlskov@hotmail.com>
2015-05-11 17:50:27 +0200

    Adding more functional tests

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/IncludeAsyncSqlServerCeTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.Entity.FunctionalTests;
+
+namespace ErikEJ.Data.Entity.SqlServerCe.FunctionalTests
+{
+    public class IncludeAsyncSqliteTest : IncludeAsyncTestBase<NorthwindQuerySqlServerCeFixture>
+    {
+        public IncludeAsyncSqliteTest(NorthwindQuerySqlServerCeFixture fixture)
+            : base(fixture)
+        {
+        }
+    }
+}
+
+

29850bc7a5762d648f73246609a561caf3630adb
ErikEJ <ejlskov@hotmail.com>
2015-05-12 19:21:10 +0200

    Implementing Historyrepo incl tests

--- src/Migrations/SqlServerCeHistoryRepository.cs
+++ src/Migrations/SqlServerCeHistoryRepository.cs
@@ -1,50 +1,165 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlServerCe;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Relational.Migrations.History;
 using Microsoft.Data.Entity.Relational.Migrations.Operations;
+using Microsoft.Data.Entity.Utilities;
 
 namespace ErikEJ.Data.Entity.SqlServerCe.Migrations
 {
     public class SqlServerCeHistoryRepository : ISqlServerCeHistoryRepository
     {
-        public virtual string BeginIfExists(string migrationId)
-        {
-            throw new NotImplementedException();
-        }
+        public const string MigrationHistoryTableName = "__MigrationHistory";
 
-        public virtual string BeginIfNotExists(string migrationId)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly ISqlServerCeConnection _connection;
+        private readonly ISqlServerCeDataStoreCreator _creator;
+        private readonly Type _contextType;
+        private readonly ISqlServerCeSqlGenerator _sql;
 
-        public virtual string Create(bool ifNotExists)
+        public SqlServerCeHistoryRepository(
+            [NotNull] ISqlServerCeConnection connection,
+            [NotNull] ISqlServerCeDataStoreCreator creator,
+            [NotNull] DbContext context,
+            [NotNull] ISqlServerCeSqlGenerator sqlGenerator)
         {
-            throw new NotImplementedException();
-        }
+            Check.NotNull(connection, nameof(connection));
+            Check.NotNull(creator, nameof(creator));
+            Check.NotNull(context, nameof(context));
+            Check.NotNull(sqlGenerator, nameof(sqlGenerator));
 
-        public virtual string EndIf()
-        {
-            throw new NotImplementedException();
+            _connection = connection;
+            _creator = creator;
+            _contextType = context.GetType();
+            _sql = sqlGenerator;
         }
 
         public virtual bool Exists()
         {
-            throw new NotImplementedException();
+            var exists = false;
+
+            if (!_creator.Exists())
+            {
+                return exists;
+            }
+
+            var command = (SqlCeCommand)_connection.DbConnection.CreateCommand();
+            command.CommandText =
+                @"SELECT 1 FROM [INFORMATION_SCHEMA].[TABLES]
+WHERE [TABLE_NAME] = '" + MigrationHistoryTableName + "' AND [TABLE_TYPE] <> N'SYSTEM TABLE'";
+
+            _connection.Open();
+            try
+            {
+                exists = command.ExecuteScalar() != null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return exists;
         }
 
         public virtual IReadOnlyList<IHistoryRow> GetAppliedMigrations()
         {
-            throw new NotImplementedException();
+            var rows = new List<HistoryRow>();
+
+            if (!Exists())
+            {
+                return rows;
+            }
+
+            _connection.Open();
+            try
+            {
+                var command = (SqlCeCommand)_connection.DbConnection.CreateCommand();
+                command.CommandText =
+                    @"SELECT [MigrationId], [ProductVersion]
+FROM [" + MigrationHistoryTableName + @"]
+WHERE [ContextKey] = @ContextKey ORDER BY [MigrationId]";
+                command.Parameters.AddWithValue("@ContextKey", _contextType.FullName);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        rows.Add(new HistoryRow(reader.GetString(0), reader.GetString(1)));
+                    }
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return rows;
+        }
+
+        public virtual string Create(bool ifNotExists)
+        {
+            var builder = new IndentedStringBuilder();
+
+            builder
+                .AppendLine("CREATE TABLE [" + MigrationHistoryTableName + "] (");
+            using (builder.Indent())
+            {
+                builder
+                    .AppendLine("[MigrationId] nvarchar(150) NOT NULL,")
+                    .AppendLine("[ContextKey] nvarchar(300) NOT NULL,")
+                    .AppendLine("[ProductVersion] nvarchar(32) NOT NULL,")
+                    .AppendLine("CONSTRAINT [PK_MigrationHistory] PRIMARY KEY ([MigrationId], [ContextKey])");
+            }
+            builder.Append(");");
+
+            return builder.ToString();
         }
 
         public virtual MigrationOperation GetDeleteOperation(string migrationId)
         {
-            throw new NotImplementedException();
+            Check.NotEmpty(migrationId, nameof(migrationId));
+
+            return new SqlOperation
+            {
+                Sql = new StringBuilder()
+                        .AppendLine("DELETE FROM [" + MigrationHistoryTableName + "]")
+                        .Append("WHERE [MigrationId] = '").Append(_sql.EscapeLiteral(migrationId))
+                        .Append("' AND [ContextKey] = '").Append(_sql.EscapeLiteral(_contextType.FullName))
+                        .AppendLine("';")
+                        .ToString()
+            };
         }
 
         public virtual MigrationOperation GetInsertOperation(IHistoryRow row)
         {
-            throw new NotImplementedException();
+            Check.NotNull(row, nameof(row));
+
+            return new SqlOperation
+            {
+                Sql = new StringBuilder()
+                        .AppendLine("INSERT INTO [" + MigrationHistoryTableName + "] ([MigrationId], [ContextKey], [ProductVersion])")
+                        .Append("VALUES ('").Append(_sql.EscapeLiteral(row.MigrationId)).Append("', '")
+                        .Append(_sql.EscapeLiteral(_contextType.FullName)).Append("', '")
+                        .Append(_sql.EscapeLiteral(row.ProductVersion)).AppendLine("');")
+                        .ToString()
+            };
+        }
+
+        public virtual string BeginIfNotExists(string migrationId)
+        {
+            return string.Empty;
+        }
+
+        public virtual string BeginIfExists(string migrationId)
+        {
+            return string.Empty;
+        }
+
+        public virtual string EndIf()
+        {
+            return string.Empty;
         }
     }
 }

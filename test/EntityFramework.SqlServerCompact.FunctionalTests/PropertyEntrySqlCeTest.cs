94b70d8afbb0a5f67c75c93fddd32c429ffb1c24
ErikEJ <ejlskov@hotmail.com>
2015-05-28 09:19:57 +0200

    To share failed test

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/PropertyEntrySqlCeTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Entity.Relational.FunctionalTests;
+using Xunit;
+
+namespace ErikEJ.Data.Entity.SqlServerCe.FunctionalTests
+{
+    public class PropertyEntrySqlCeTest : PropertyEntryTestBase<SqlCeTestStore, F1SqlCeFixture>
+    {
+        public PropertyEntrySqlCeTest(F1SqlCeFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        public override void Property_entry_original_value_is_set()
+        {
+            base.Property_entry_original_value_is_set();
+
+            Assert.Contains(
+                @"SELECT ""e"".""Id"", ""e"".""EngineSupplierId"", ""e"".""Name""
+FROM ""Engines"" AS ""e""
+LIMIT 1",
+                Sql);
+
+            Assert.Contains(
+                @"UPDATE ""Engines"" SET ""Name"" = @p0
+WHERE ""Id"" = @p1 AND ""EngineSupplierId"" = @p2 AND ""Name"" = @p3;
+SELECT changes();",
+                Sql);
+        }
+
+        private static string Sql => TestSqlLoggerFactory.Sql;
+    }
+}

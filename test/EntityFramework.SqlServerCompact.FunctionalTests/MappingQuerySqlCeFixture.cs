43670436711705a623db89b73d16f300a3bbc425
ErikEJ <ejlskov@hotmail.com>
2015-05-29 07:59:17 +0200

    adding more tests

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/MappingQuerySqlCeFixture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ErikEJ.Data.Entity.SqlServerCe.FunctionalTests.TestModels;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.FunctionalTests;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Relational.FunctionalTests;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+
+namespace ErikEJ.Data.Entity.SqlServerCe.FunctionalTests
+{
+    public class MappingQuerySqlCeFixture : MappingQueryFixtureBase
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly EntityOptions _options;
+        private readonly SqlCeTestStore _testDatabase;
+
+        public MappingQuerySqlCeFixture()
+        {
+            _serviceProvider = new ServiceCollection()
+                .AddEntityFramework()
+                .AddSqlCe()
+                .ServiceCollection()
+                .AddInstance<ILoggerFactory>(new TestSqlLoggerFactory())
+                .BuildServiceProvider();
+
+            _testDatabase = SqlCeNorthwindContext.GetSharedStore();
+
+            var optionsBuilder = new EntityOptionsBuilder().UseModel(CreateModel());
+            optionsBuilder.UseSqlCe(_testDatabase.Connection.ConnectionString);
+            _options = optionsBuilder.Options;
+        }
+
+        public DbContext CreateContext()
+        {
+            return new DbContext(_serviceProvider, _options);
+        }
+
+        public void Dispose()
+        {
+            _testDatabase.Dispose();
+        }
+
+        protected override string DatabaseSchema { get; } = null;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MappingQueryTestBase.MappedCustomer>(e =>
+            {
+                // TODO: Use .SqlCe() when available - if it is not removed!
+                e.Property(c => c.CompanyName2).Metadata.Relational().Column = "CompanyName";
+                e.Metadata.Relational().Table = "Customers";
+            });
+        }
+    }
+}
---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/MappingQuerySqlCeTest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Relational.FunctionalTests;
+using Xunit;
+
+namespace ErikEJ.Data.Entity.SqlServerCe.FunctionalTests
+{
+    public class MappingQuerySqlCeTest : MappingQueryTestBase, IClassFixture<MappingQuerySqlCeFixture>
+    {
+        public override void All_customers()
+        {
+            base.All_customers();
+
+            Assert.Equal(
+                @"SELECT [c].[CustomerID], [c].[CompanyName]
+FROM [Customers] AS [c]",
+                Sql);
+        }
+
+        public override void All_employees()
+        {
+            base.All_employees();
+
+            Assert.Equal(
+                @"SELECT [e].[EmployeeID], [e].[City]
+FROM [Employees] AS [e]",
+                Sql);
+        }
+
+        public override void All_orders()
+        {
+            base.All_orders();
+
+            Assert.Equal(
+                @"SELECT [o].[OrderID], [o].[ShipVia]
+FROM [Orders] AS [o]",
+                Sql);
+        }
+
+        public override void Project_nullable_enum()
+        {
+            base.Project_nullable_enum();
+
+            Assert.Equal(
+                @"SELECT [o].[ShipVia]
+FROM [Orders] AS [o]",
+                Sql);
+        }
+
+        private readonly MappingQuerySqlCeFixture _fixture;
+
+        public MappingQuerySqlCeTest(MappingQuerySqlCeFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        protected override DbContext CreateContext()
+        {
+            return _fixture.CreateContext();
+        }
+
+        private static string Sql
+        {
+            get { return TestSqlLoggerFactory.Sql; }
+        }
+    }
+}

1db2858e2cf4e710bd1a1b4162abd6f73ff1d4d1
ErikEJ <ejlskov@hotmail.com>
2015-07-25 16:24:16 +0200

    Some 7s were missed

---
+++ NuGetBuild.cmd
@@ -0,0 +1,7 @@
+.nuget\nuget pack .\src\design35\EntityFramework.SqlServerCompact35.Design.nuspec -version 7.0.0-beta6
+.nuget\nuget pack .\src\provider35\EntityFramework.SqlServerCompact35.nuspec -version 7.0.0-beta6
+
+.nuget\nuget pack .\src\design40\EntityFramework.SqlServerCompact40.Design.nuspec -version 7.0.0-beta6
+.nuget\nuget pack .\src\provider40\EntityFramework.SqlServerCompact40.nuspec -version 7.0.0-beta6
+
+pause

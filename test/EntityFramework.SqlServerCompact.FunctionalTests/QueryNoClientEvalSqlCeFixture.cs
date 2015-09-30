ac443cf9e1ec3d820b0dcc45c64316a4df6081bf
ErikEJ <ejlskov@hotmail.com>
2015-09-30 16:08:00 +0200

    Add client eval tests

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/QueryNoClientEvalSqlCeFixture.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.Entity.Infrastructure;
+
+namespace ErikEJ.Data.Entity.SqlServerCe.FunctionalTests
+{
+    public class QueryNoClientEvalSqlCeFixture : NorthwindQuerySqlCeFixture
+    {
+        protected override void ConfigureOptions(SqlCeDbContextOptionsBuilder sqlCeDbContextOptionsBuilder)
+            => sqlCeDbContextOptionsBuilder.DisableQueryClientEvaluation();
+    }
+}

9a11c79472a7d432392c5d7595cc48724b90da80
ErikEJ <ejlskov@hotmail.com>
2015-12-26 17:04:42 +0100

    Adding project.json base test project for debugging within the EF codebase

---
+++ test/EntityFramework.SqlCe.FunctionalTests/NorthwindQuerySqlCeFixture.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Data.Entity.FunctionalTests.TestModels;
+using Microsoft.Data.Entity.FunctionalTests.TestModels.Northwind;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Data.Entity.FunctionalTests
+{
+    public class NorthwindQuerySqlCeFixture : NorthwindQueryRelationalFixture, IDisposable
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly DbContextOptions _options;
+
+        private readonly SqlCeTestStore _testStore = SqlCeNorthwindContext.GetSharedStore();
+        private readonly TestSqlLoggerFactory _testSqlLoggerFactory = new TestSqlLoggerFactory();
+
+        public NorthwindQuerySqlCeFixture()
+        {
+            _serviceProvider
+                = new ServiceCollection()
+                    .AddEntityFramework()
+                    .AddSqlCe()
+                    .ServiceCollection()
+                    .AddSingleton(TestSqlCeModelSource.GetFactory(OnModelCreating))
+                    .AddSingleton<ILoggerFactory>(_testSqlLoggerFactory)
+                    .BuildServiceProvider();
+
+            _options = BuildOptions();
+        }
+
+        protected DbContextOptions BuildOptions()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+
+            var sqlCeDbContextOptionsBuilder
+                = optionsBuilder
+                    .EnableSensitiveDataLogging()
+                    .UseSqlCe(_testStore.Connection.ConnectionString);
+
+            ConfigureOptions(sqlCeDbContextOptionsBuilder);
+
+            return optionsBuilder.Options;
+        }
+
+        protected virtual void ConfigureOptions(SqlCeDbContextOptionsBuilder sqlServerDbContextOptionsBuilder)
+        {
+        }
+
+        public override NorthwindContext CreateContext()
+            => new SqlCeNorthwindContext(_serviceProvider, _options);
+
+        public void Dispose() => _testStore.Dispose();
+
+        public override CancellationToken CancelQuery() => _testSqlLoggerFactory.CancelQuery();
+    }
+}

9a273b045d2649a326d082496c14cbe741947909
ErikEJ <ejlskov@hotmail.com>
2016-01-20 17:55:52 +0100

    Refresh to "latest" (Jan 12) RC2 bits

--- src/Provider40/Query/Sql/Internal/SqlCeQuerySqlGenerator.cs
+++ src/Provider40/Query/Sql/Internal/SqlCeQuerySqlGenerator.cs
@@ -1,12 +1,14 @@
 ﻿#if SQLCE35
 using System;
 #endif
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using JetBrains.Annotations;
 using Microsoft.Data.Entity.Query.Expressions;
 using Microsoft.Data.Entity.Storage;
 using Microsoft.Data.Entity.Utilities;
+using Remotion.Linq.Parsing;
 
 namespace Microsoft.Data.Entity.Query.Sql.Internal
 {
@@ -61,6 +63,75 @@ namespace Microsoft.Data.Entity.Query.Sql.Internal
             }
 
             base.GenerateLimitOffset(selectExpression);
+        }
+
+        protected override void VisitProjection(IReadOnlyList<Expression> projections)
+        {
+            var comparisonTransformer = new ProjectionComparisonTransformingVisitor();
+            var transformedProjections = projections.Select(comparisonTransformer.Visit).ToList();
+
+            base.VisitProjection(transformedProjections);
+        }
+
+        private class ProjectionComparisonTransformingVisitor : RelinqExpressionVisitor
+        {
+            private bool _insideConditionalTest = false;
+
+            protected override Expression VisitUnary(UnaryExpression node)
+            {
+                if (!_insideConditionalTest
+                    && node.NodeType == ExpressionType.Not
+                    && node.Operand is AliasExpression)
+                {
+                    return Expression.Condition(
+                        node,
+                        Expression.Constant(true, typeof(bool)),
+                        Expression.Constant(false, typeof(bool)));
+                }
+
+                return base.VisitUnary(node);
+            }
+
+            protected override Expression VisitBinary(BinaryExpression node)
+            {
+                if (!_insideConditionalTest
+                    && (node.IsComparisonOperation()
+                        || node.IsLogicalOperation()))
+                {
+                    return Expression.Condition(
+                        node,
+                        Expression.Constant(true, typeof(bool)),
+                        Expression.Constant(false, typeof(bool)));
+                }
+
+                return base.VisitBinary(node);
+            }
+
+            protected override Expression VisitConditional(ConditionalExpression node)
+            {
+                _insideConditionalTest = true;
+                var test = Visit(node.Test);
+                _insideConditionalTest = false;
+                if (test is AliasExpression)
+                {
+                    return Expression.Condition(
+                        Expression.Equal(test, Expression.Constant(true, typeof(bool))),
+                        Visit(node.IfTrue),
+                        Visit(node.IfFalse));
+                }
+
+                var condition = test as ConditionalExpression;
+                if (condition != null)
+                {
+                    return Expression.Condition(
+                        condition.Test,
+                        Visit(node.IfTrue),
+                        Visit(node.IfFalse));
+                }
+                return Expression.Condition(test,
+                    Visit(node.IfTrue),
+                    Visit(node.IfFalse));
+            }
         }
     }
 }

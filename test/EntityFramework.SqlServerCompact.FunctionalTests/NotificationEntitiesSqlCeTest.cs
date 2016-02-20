5642df5fdf8a19f5af2c53627a3d18f3b49b363a
ErikEJ <ejlskov@hotmail.com>
2016-02-20 23:53:56 +0100

    Fixes #310

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/NotificationEntitiesSqlCeTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.FunctionalTests;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.EntityFrameworkCore.SqlCe.FunctionalTests
+{
+    public class NotificationEntitiesSqlCeTest
+        : NotificationEntitiesTestBase<NotificationEntitiesSqlCeTest.NotificationEntitiesSqlCeFixture>
+    {
+        public NotificationEntitiesSqlCeTest(NotificationEntitiesSqlCeFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        public class NotificationEntitiesSqlCeFixture : NotificationEntitiesFixtureBase
+        {
+            private readonly IServiceProvider _serviceProvider;
+            private readonly DbContextOptions _options;
+
+            public NotificationEntitiesSqlCeFixture()
+            {
+                _serviceProvider = new ServiceCollection()
+                    .AddEntityFramework()
+                    .AddSqlCe()
+                    .ServiceCollection()
+                    .AddSingleton(TestSqlCeModelSource.GetFactory(OnModelCreating))
+                    .BuildServiceProvider();
+
+                var optionsBuilder = new DbContextOptionsBuilder();
+                optionsBuilder.UseSqlCe(SqlCeTestStore.CreateConnectionString("NotificationEntities"));
+                _options = optionsBuilder.Options;
+
+                EnsureCreated();
+            }
+
+            public override DbContext CreateContext()
+                => new DbContext(_serviceProvider, _options);
+        }
+    }
+}

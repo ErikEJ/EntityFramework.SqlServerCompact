0c8fe9a0906007b25a1413b4ff0963cd5baab94a
ErikEJ <ejlskov@hotmail.com>
2016-05-15 14:19:33 +0200

    Date part - initial work

--- src/Provider40/Query/ExpressionTranslators/Internal/SqlCeCompositeMemberTranslator.cs
+++ src/Provider40/Query/ExpressionTranslators/Internal/SqlCeCompositeMemberTranslator.cs
@@ -9,7 +9,11 @@ namespace Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal
             var sqlCeTranslators = new List<IMemberTranslator>
             {
                 new SqlCeStringLengthTranslator(),
-                new SqlCeDateTimeNowTranslator()
+                new SqlCeDateTimeNowTranslator(),
+                new SqlCeDateTimeDateComponentTranslator()
+                //TODO ErikEJ Implement when support in daily build
+                //,
+                //new SqlCeDateTimeDatePartComponentTranslator(),
             };
 
             // ReSharper disable once VirtualMemberCallInContructor

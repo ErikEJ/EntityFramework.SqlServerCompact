59605d4fbc2d13d165dd54310df86b30383f4bc6
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2016-10-15 15:34:00 +0200

    Fixes #349 (#373)
    Also adds check for consistency with design dll

---
+++ test/EntityFramework.SqlServerCompact40.Design.FunctionalTest/SqlCeDesignTimeProviderServicesTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Relational.Design.Specification.Tests;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+using Microsoft.EntityFrameworkCore.Storage.Internal;
+
+namespace EntityFramework.SqlServerCompact40.Design.FunctionalTest
+{
+    public class SqlCeDesignTimeProviderServicesTest : DesignTimeProviderServicesTest
+    {
+        protected override Assembly GetRuntimeAssembly()
+            => typeof(SqlCeDatabaseConnection).GetTypeInfo().Assembly;
+
+        protected override Type GetDesignTimeServicesType()
+            => typeof(SqlCeDesignTimeServices);
+    }
+}

f44811315487683b9382134b792eb587021545a5
ErikEJ <ejlskov@hotmail.com>
2017-04-07 17:50:35 +0200

    Update to version 2.0

--- src/Provider40/Properties/AssemblyInfo.cs
+++ src/Provider40/Properties/AssemblyInfo.cs
@@ -6,16 +6,16 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 [assembly: AssemblyProduct("EntityFrameworkCore.SqlServerCompact35")]
 [assembly: DesignTimeProviderServices(
     typeName: "Microsoft.EntityFrameworkCore.Scaffolding.Internal.SqlCeDesignTimeServices",
-    assemblyName: "EntityFrameworkCore.SqlServerCompact35.Design, Version=1.2.0.0, Culture=neutral, PublicKeyToken=9af395b34ac99006",
+    assemblyName: "EntityFrameworkCore.SqlServerCompact35.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=9af395b34ac99006",
     packageName: "EntityFrameworkCore.SqlServerCompact35.Design")]
 #else
 [assembly: AssemblyTitle("EntityFrameworkCore.SqlServerCompact40")]
 [assembly: AssemblyProduct("EntityFrameworkCore.SqlServerCompact40")]
 [assembly: DesignTimeProviderServices(
     typeName: "Microsoft.EntityFrameworkCore.Scaffolding.Internal.SqlCeDesignTimeServices",
-    assemblyName: "EntityFrameworkCore.SqlServerCompact40.Design, Version=1.2.0.0, Culture=neutral, PublicKeyToken=9af395b34ac99006",
+    assemblyName: "EntityFrameworkCore.SqlServerCompact40.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=9af395b34ac99006",
     packageName: "EntityFrameworkCore.SqlServerCompact40.Design")]
 #endif
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0-preview1")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-preview1")]

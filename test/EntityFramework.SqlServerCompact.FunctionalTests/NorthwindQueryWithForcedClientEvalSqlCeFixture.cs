0bbae9ebe3c066e33ca721d03455e788d1d2f95a
ErikEJ <ejlskov@hotmail.com>
2017-04-16 14:02:45 +0200

    Add test of ClientEvalForUnsupportedSqlConstructs

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/NorthwindQueryWithForcedClientEvalSqlCeFixture.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Specification.Tests.TestModels;
+using Microsoft.EntityFrameworkCore.Specification.Tests.TestModels.Northwind;
+using Microsoft.EntityFrameworkCore.Specification.Tests.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.EntityFrameworkCore.Specification.Tests
+{
+    public class NorthwindQueryWithForcedClientEvalSqlCeFixture : NorthwindQueryRelationalFixture, IDisposable
+    {
+        private readonly DbContextOptions _options;
+
+        private readonly SqlCeTestStore _testStore = SqlCeNorthwindContext.GetSharedStore();
+        private readonly TestSqlLoggerFactory _testSqlLoggerFactory = new TestSqlLoggerFactory();
+
+        public NorthwindQueryWithForcedClientEvalSqlCeFixture()
+        {
+            _options = BuildOptions();
+        }
+
+        public override DbContextOptions BuildOptions(IServiceCollection additionalServices = null)
+            => ConfigureOptions(
+                new DbContextOptionsBuilder()
+                    .EnableSensitiveDataLogging()
+                    .UseInternalServiceProvider((additionalServices ?? new ServiceCollection())
+                        .AddEntityFrameworkSqlCe()
+                        .AddSingleton(TestModelSource.GetFactory(OnModelCreating))
+                        .AddSingleton<ILoggerFactory>(_testSqlLoggerFactory)
+                        .BuildServiceProvider()))
+                .UseSqlCe(
+                    _testStore.ConnectionString,
+                    b =>
+                    {
+                        ConfigureOptions(b);
+                        b.ApplyConfiguration();
+                        b.UseClientEvalForUnsupportedSqlConstructs(true);
+                    })
+                    .Options;
+
+        protected virtual DbContextOptionsBuilder ConfigureOptions(DbContextOptionsBuilder dbContextOptionsBuilder)
+            => dbContextOptionsBuilder;
+
+        protected virtual void ConfigureOptions(SqlCeDbContextOptionsBuilder sqlCeDbContextOptionsBuilder)
+        {
+        }
+
+        public override NorthwindContext CreateContext(
+            QueryTrackingBehavior queryTrackingBehavior = QueryTrackingBehavior.TrackAll)
+            => new SqlCeNorthwindContext(_options, queryTrackingBehavior);
+
+        public void Dispose() => _testStore.Dispose();
+
+        public override CancellationToken CancelQuery() => _testSqlLoggerFactory.CancelQuery();
+    }
+}
---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/QueryNavigationsWithForcedClientEvalSqlCeTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Specification.Tests.Utilities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.EntityFrameworkCore.Specification.Tests
+{
+    public class QueryNavigationsWithForcedClientEvalSqlCeTest : QueryNavigationsTestBase<NorthwindQueryWithForcedClientEvalSqlCeFixture>
+    {
+        public QueryNavigationsWithForcedClientEvalSqlCeTest(
+            // ReSharper disable once UnusedParameter.Local
+            NorthwindQueryWithForcedClientEvalSqlCeFixture fixture, ITestOutputHelper testOutputHelper)
+            : base(fixture)
+        {
+            //TestSqlLoggerFactory.CaptureOutput(testOutputHelper);
+        }
+
+        [Fact(Skip ="Investigate why this fails with forced client eval")]
+        public override void Where_subquery_on_navigation()
+        {
+            base.Where_subquery_on_navigation();
+        }
+
+        [Fact(Skip = "Investigate why this fails with forced client eval")]
+        public override void Where_subquery_on_navigation2()
+        {
+            base.Where_subquery_on_navigation2();
+        }
+
+        protected override void ClearLog() => TestSqlLoggerFactory.Reset();
+
+        private const string FileLineEnding = @"
+";
+
+        private static string Sql => TestSqlLoggerFactory.Sql.Replace(Environment.NewLine, FileLineEnding);
+    }
+}

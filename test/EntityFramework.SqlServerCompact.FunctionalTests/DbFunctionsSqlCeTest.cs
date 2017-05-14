7587b9685aa96f327cde717ddd9b45937f23884a
ErikEJ <ejlskov@hotmail.com>
2017-05-14 13:39:39 +0200

    Adding 2 missing tests
    fixes #450

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/DbFunctionsSqlCeTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Specification.Tests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.EntityFrameworkCore.SqlServer.FunctionalTests
+{
+    public class DbFunctionsSqlCeTest : DbFunctionsTestBase<NorthwindQuerySqlCeFixture>
+    {
+        public DbFunctionsSqlCeTest(NorthwindQuerySqlCeFixture fixture, ITestOutputHelper testOutputHelper)
+            : base(fixture)
+        {
+            fixture.TestSqlLoggerFactory.Clear();
+        }
+
+        public override void String_Like_Literal()
+        {
+            base.String_Like_Literal();
+
+            Assert.Equal(
+                @"SELECT COUNT(*)
+FROM [Customers] AS [c]
+WHERE [c].[ContactName] LIKE N'%M%'",
+                Sql);
+        }
+
+        public override void String_Like_Identity()
+        {
+            base.String_Like_Identity();
+
+            Assert.Equal(
+                @"SELECT COUNT(*)
+FROM [Customers] AS [c]
+WHERE [c].[ContactName] LIKE [c].[ContactName]",
+                Sql);
+        }
+
+        public override void String_Like_Literal_With_Escape()
+        {
+            base.String_Like_Literal_With_Escape();
+
+            Assert.Equal(
+                @"SELECT COUNT(*)
+FROM [Customers] AS [c]
+WHERE [c].[ContactName] LIKE N'!%' ESCAPE '!'",
+                Sql);
+        }
+
+        private const string FileLineEnding = @"
+";
+
+        private string Sql => Fixture.TestSqlLoggerFactory.Sql.Replace(Environment.NewLine, FileLineEnding);
+    }
+}

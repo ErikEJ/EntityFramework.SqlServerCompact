129fa8d73a9a130364538513ba6825d634bf6b3c
ErikEJ <ejlskov@hotmail.com>
2017-08-06 14:50:22 +0200

    Enable MonsterFixUp test
    fixes #452

--- test/EntityFramework.SqlServerCompact.FunctionalTests/MonsterFixUpSqlCeTest.cs
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/MonsterFixUpSqlCeTest.cs
@@ -3,64 +3,65 @@ using System.Data.SqlServerCe;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using Microsoft.EntityFrameworkCore.Specification.Tests.TestModels;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.TestModels;
 
 namespace Microsoft.EntityFrameworkCore.Specification.Tests
 {
-    //public class MonsterFixupSqlCeTest : MonsterFixupTestBase
-    //{
-    //    protected override IServiceProvider CreateServiceProvider(bool throwingStateManager = false)
-    //    {
-    //        var serviceCollection = new ServiceCollection()
-    //            .AddEntityFrameworkSqlCe();
+    public class MonsterFixupSqlCeTest : MonsterFixupTestBase
+    {
+        protected override IServiceProvider CreateServiceProvider(bool throwingStateManager = false)
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddEntityFrameworkSqlCe();
 
-    //        if (throwingStateManager)
-    //        {
-    //            serviceCollection.AddScoped<IStateManager, ThrowingMonsterStateManager>();
-    //        }
+            if (throwingStateManager)
+            {
+                serviceCollection.AddScoped<IStateManager, ThrowingMonsterStateManager>();
+            }
 
-    //        return serviceCollection.BuildServiceProvider();
-    //    }
+            return serviceCollection.BuildServiceProvider();
+        }
 
-    //    protected override DbContextOptions CreateOptions(string databaseName)
-    //    {
-    //        var optionsBuilder = new DbContextOptionsBuilder();
-    //        optionsBuilder
-    //            .UseSqlCe(CreateConnectionString(databaseName));
+        protected override DbContextOptions CreateOptions(string databaseName)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder
+                .UseSqlCe(CreateConnectionString(databaseName));
 
-    //        return optionsBuilder.Options;
-    //    }
+            return optionsBuilder.Options;
+        }
 
-    //    private static string CreateConnectionString(string name)
-    //    {
-    //        return new SqlCeConnectionStringBuilder
-    //        {
-    //            DataSource = name + ".sdf"
-    //        }.ConnectionString;
-    //    }
+        private static string CreateConnectionString(string name)
+        {
+            return new SqlCeConnectionStringBuilder
+            {
+                DataSource = name + ".sdf"
+            }.ConnectionString;
+        }
 
-    //    private SqlCeTestStore _testStore;
+        private SqlCeTestStore _testStore;
 
-    //    protected override void CreateAndSeedDatabase(string databaseName, Func<MonsterContext> createContext, Action<MonsterContext> seed)
-    //    {
-    //        _testStore = SqlCeTestStore.GetOrCreateShared(databaseName, () =>
-    //        {
-    //            using (var context = createContext())
-    //            {
-    //                context.Database.EnsureCreated();
-    //                seed(context);
-    //            }
-    //        });
-    //    }
+        protected override void CreateAndSeedDatabase(string databaseName, Func<MonsterContext> createContext, Action<MonsterContext> seed)
+        {
+            _testStore = SqlCeTestStore.GetOrCreateShared(databaseName, () =>
+            {
+                using (var context = createContext())
+                {
+                    context.Database.EnsureCreated();
+                    seed(context);
+                }
+            });
+        }
 
-    //    public virtual void Dispose() => _testStore?.Dispose();
+        public virtual void Dispose() => _testStore?.Dispose();
 
-    //    public override void OnModelCreating<TMessage, TProductPhoto, TProductReview>(ModelBuilder builder)
-    //    {
-    //        base.OnModelCreating<TMessage, TProductPhoto, TProductReview>(builder);
+        protected override void OnModelCreating<TMessage, TProductPhoto, TProductReview>(ModelBuilder builder)
+        {
+            base.OnModelCreating<TMessage, TProductPhoto, TProductReview>(builder);
 
-    //        builder.Entity<TMessage>().HasKey(e => e.MessageId);
-    //        builder.Entity<TProductPhoto>().HasKey(e => e.PhotoId);
-    //        builder.Entity<TProductReview>().HasKey(e => e.ReviewId);
-    //    }
-    //}
+            builder.Entity<TMessage>().HasKey(e => e.MessageId);
+            builder.Entity<TProductPhoto>().HasKey(e => e.PhotoId);
+            builder.Entity<TProductReview>().HasKey(e => e.ReviewId);
+        }
+    }
 }

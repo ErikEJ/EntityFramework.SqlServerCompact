79dabdc8679486675953e117e5bd89b455ffbd4f
ErikEJ <ejlskov@hotmail.com>
2018-03-16 14:28:30 +0100

    Fix default namespace

--- src/Provider40/Strings.Designer.cs
+++ src/Provider40/Strings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.EntityFrameworkCore {
+namespace EFCore.SqlCe {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Microsoft.EntityFrameworkCore {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.EntityFrameworkCore.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EFCore.SqlCe.Strings", typeof(Strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

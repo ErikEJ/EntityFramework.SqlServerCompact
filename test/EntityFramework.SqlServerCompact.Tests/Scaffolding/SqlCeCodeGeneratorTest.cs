a97a3b1eefb15b8b4c49d384b144ecbf8834c7e5
ErikEJ <ejlskov@hotmail.com>
2018-03-29 10:37:10 +0200

    Add small test

---
+++ test/EntityFramework.SqlServerCompact.Tests/Scaffolding/SqlCeCodeGeneratorTest.cs
@@ -0,0 +1,16 @@
+﻿using EFCore.SqlCe.Scaffolding.Internal;
+using Xunit;
+
+namespace Microsoft.EntityFrameworkCore.Scaffolding
+{
+    public class SqlCeCodeGeneratorTest
+    {
+        [Fact]
+        public virtual void Use_provider_method_is_generated_correctly()
+        {
+            var codeGenerator = new SqlCeCodeGenerator(new ProviderCodeGeneratorDependencies());
+
+            Assert.Equal("UseSqlCe", codeGenerator.UseProviderMethod);
+        }
+    }
+}

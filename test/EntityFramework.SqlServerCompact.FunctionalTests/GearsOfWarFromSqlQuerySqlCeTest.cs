7bbb8e1002feade821c7bbdea71f4d09b674da3e
ErikEJ <ejlskov@hotmail.com>
2018-07-29 21:02:17 +0200

    Add more compliance tests

---
+++ test/EntityFramework.SqlServerCompact.FunctionalTests/GearsOfWarFromSqlQuerySqlCeTest.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.EntityFrameworkCore.Query
+{
+    public class GearsOfWarFromSqlQuerySqlCeTest : GearsOfWarFromSqlQueryTestBase<GearsOfWarQuerySqlCeFixture>
+    {
+        public GearsOfWarFromSqlQuerySqlCeTest(GearsOfWarQuerySqlCeFixture fixture, ITestOutputHelper testOutputHelper)
+            : base(fixture)
+        {
+            fixture.TestSqlLoggerFactory.Clear();
+        }
+
+        public override void From_sql_queryable_simple_columns_out_of_order()
+        {
+            base.From_sql_queryable_simple_columns_out_of_order();
+
+            Assert.Equal(
+                @"SELECT ""Id"", ""Name"", ""IsAutomatic"", ""AmmunitionType"", ""OwnerFullName"", ""SynergyWithId"" FROM ""Weapons"" ORDER BY ""Name""",
+                Sql);
+        }
+
+        protected override void ClearLog()
+            => Fixture.TestSqlLoggerFactory.Clear();
+
+        private string Sql => Fixture.TestSqlLoggerFactory.Sql;
+    }
+}
